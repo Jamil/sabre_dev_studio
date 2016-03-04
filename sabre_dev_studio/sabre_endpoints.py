fd12815f1a8b7f73421d08c179c8733367736294
jamil <jamil.dhanani@gmail.com>
2016-03-03 20:52:04 -0500

    Add seat map endpoint

--- sabre_dev_studio/sabre_endpoints.py
+++ sabre_dev_studio/sabre_endpoints.py
@@ -1,4 +1,5 @@
 sabre_endpoints = {
     'instaflights': '/v1/shop/flights',
-    'flights_to': '/v1/shop/flights/cheapest/fares'
+    'flights_to': '/v1/shop/flights/cheapest/fares',
+    'seat_map': '/v3.0.0/book/flights/seatmaps'
 }
