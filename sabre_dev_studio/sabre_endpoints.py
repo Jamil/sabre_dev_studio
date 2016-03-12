82cbd29d04a114f13e24a91460688772650076f7
jamil <jamil.dhanani@gmail.com>
2016-03-11 22:42:47 -0500

    Add Lead Price endpoint

--- sabre_dev_studio/sabre_endpoints.py
+++ sabre_dev_studio/sabre_endpoints.py
@@ -1,5 +1,6 @@
 sabre_endpoints = {
     'instaflights': '/v1/shop/flights',
     'flights_to': '/v1/shop/flights/cheapest/fares',
-    'seat_map': '/v3.0.0/book/flights/seatmaps?mode=seatmaps'
+    'seat_map': '/v3.0.0/book/flights/seatmaps?mode=seatmaps',
+    'lead_price': '/v2/shop/flights/fares'
 }
