1e8f53e97f24a52f2b9673e5bdd9d215dcc4fa70
jamil <jamil.dhanani@gmail.com>
2016-03-20 17:25:40 -0400

    Add Destination Finder endpoint

--- sabre_dev_studio/sabre_endpoints.py
+++ sabre_dev_studio/sabre_endpoints.py
@@ -2,5 +2,6 @@ sabre_endpoints = {
     'instaflights': '/v1/shop/flights',
     'flights_to': '/v1/shop/flights/cheapest/fares',
     'seat_map': '/v3.0.0/book/flights/seatmaps?mode=seatmaps',
-    'lead_price': '/v2/shop/flights/fares'
+    'lead_price': '/v2/shop/flights/fares',
+    'destination_finder': '/v2/shop/flights/fares' 
 }
