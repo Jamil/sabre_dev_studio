a84849253e9ddf61ac02b3bd64c7d2ee7bfdd2ea
jamil <jamil.dhanani@gmail.com>
2016-03-21 14:13:50 -0400

    Add geocode endpoint

--- sabre_dev_studio/sabre_endpoints.py
+++ sabre_dev_studio/sabre_endpoints.py
@@ -3,5 +3,6 @@ sabre_endpoints = {
     'flights_to': '/v1/shop/flights/cheapest/fares',
     'seat_map': '/v3.0.0/book/flights/seatmaps?mode=seatmaps',
     'lead_price': '/v2/shop/flights/fares',
-    'destination_finder': '/v2/shop/flights/fares' 
+    'destination_finder': '/v2/shop/flights/fares',
+    'geo_code': '/v1/lists/utilities/geocode/locations'
 }
