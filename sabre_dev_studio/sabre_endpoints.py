4458cf72ac30fc6a944b668480a23875c680e5f9
jamil <jamil.dhanani@gmail.com>
2016-03-30 16:23:46 -0400

    Add top destinations endpoint

--- sabre_dev_studio/sabre_endpoints.py
+++ sabre_dev_studio/sabre_endpoints.py
@@ -1,8 +1,12 @@
 sabre_endpoints = {
+    # Air Search
     'instaflights': '/v1/shop/flights',
     'flights_to': '/v1/shop/flights/cheapest/fares',
     'seat_map': '/v3.0.0/book/flights/seatmaps?mode=seatmaps',
     'lead_price': '/v2/shop/flights/fares',
     'destination_finder': '/v2/shop/flights/fares',
-    'geo_code': '/v1/lists/utilities/geocode/locations'
+    'geo_code': '/v1/lists/utilities/geocode/locations',
+
+    # Air Intelligence
+    'top_destinations': '/v1/lists/top/destinations'
 }
